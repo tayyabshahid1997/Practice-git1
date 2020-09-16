82b1cb4d3836f71351d3d7b6ec1b97d97c24acac
Tayyab Shahid <tayyab_shahid@outlook.com>
2020-09-16 15:04:52 +0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewREpossitory.Startup))]
+namespace NewREpossitory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
